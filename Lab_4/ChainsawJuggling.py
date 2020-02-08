dd476d6089f8dfdef4511a2dc7d4684502530b67
Molly Clabots <molly.clabots@gmail.com>
2020-02-08 13:40:09 -0600

    Adding menu to program

---
+++ Lab_4/ChainsawJuggling.py
@@ -0,0 +1,17 @@
+from peewee import *
+
+def main():
+    choice = ' '
+    while choice.upper()!= 'Q':
+        choice = input()
+
+def add_new_record_holder():
+
+def search_for_RH():
+
+def update_record():
+
+def delete_record():
+
+if __name__ == '__main__':
+    main()
