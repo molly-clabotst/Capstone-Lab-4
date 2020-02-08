2f2c2b937999cf592b5ee209c8459661161be528
Molly Clabots <molly.clabots@gmail.com>
2020-02-08 16:17:39 -0600

    Adding search function

--- Lab_4/ChainsawJuggling.py
+++ Lab_4/ChainsawJuggling.py
@@ -5,13 +5,13 @@ db = SqliteDatabase('jugglers.sqlite')
 class Records(Model):
     name = CharField()
     country = CharField()
-    record = IntegerField()
+    rec = CharField()
 
     class Meta:
         database = db
     
     def __str__(self):
-        return f"{self.name} from {self.country}s' record is {self.record}."
+        return f"{self.name} from {self.country}s' record is {self.rec}."
 
 db.connect()
 db.create_tables([Records])
@@ -23,8 +23,8 @@ def main():
         print()
         if choice == '1':
             add_new_record_holder()
-        # elif choice == 2:
-        #     search_for_RH()
+        elif choice == '2':
+            search_for_RH()
         # elif choice == 3:
         #     update_record()
         # elif choice == 4:
@@ -37,15 +37,25 @@ def add_new_record_holder():
     country = input('What country does this person come from? ')
     record = input('What is their record for catches? ')
     print()
-    if len(name)<2 or len(country)<4 or record.isnumeric==False or len(record)==0:
+    if len(name)<2 or len(country)<2 or record.isnumeric==False or len(record)==0:
         print('Please enter full name, country name and a number to input a record.\n')
         return
-    juggler = Records(name=name, country=country, record=record)
+    juggler = Records(name=name, country=country, rec=record)
     juggler.save()
-    print(f"{juggler.name}s' has been saved")
+    print(f"{juggler.name}s' has been saved\n")
 
-# def search_for_RH():
+def search_for_RH():
+    name = input('What is the name of the juggler? ')
+    while len(name)<2:
+        name = input('Please enter the full name of the Chainsaw Juggler\n')    
+    
+    data = Records.select().where(Records.name==name).limit(1)
+    if data.exists() == False:
+        print(f'\nSorry there was no entry for {name}.')
 
+    for dPoint in data:
+        print('\n'+str(dPoint)+'\n')
+    
 
 # def update_record():
 
