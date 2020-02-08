5197615ebbbf8548681d886e2265e79b6e41f186
Molly Clabots <molly.clabots@gmail.com>
2020-02-08 14:36:08 -0600

    Adding 'add_new_record_holder' method

--- Lab_4/ChainsawJuggling.py
+++ Lab_4/ChainsawJuggling.py
@@ -1,17 +1,55 @@
 from peewee import *
 
+db = SqliteDatabase('jugglers.sqlite')
+
+class Records(Model):
+    name = CharField()
+    country = CharField()
+    record = IntegerField()
+
+    class Meta:
+        database = db
+    
+    def __str__(self):
+        return f"{self.name} from {self.country}s' record is {self.record}."
+
+db.connect()
+db.create_tables([Records])
+
 def main():
     choice = ' '
     while choice.upper()!= 'Q':
-        choice = input()
+        choice = input('Would you like to...\n1.Add New Record Holder\n2.Search For Record Holder\n3.Update A Record\n4.Delete a Record\nQ.Quit\n\n')
+        print()
+        if choice == '1':
+            add_new_record_holder()
+        # elif choice == 2:
+        #     search_for_RH()
+        # elif choice == 3:
+        #     update_record()
+        # elif choice == 4:
+        #     delete_record()
+
+    print('\nThanks for using the Chainsaw Juggling Record Database\n')
 
 def add_new_record_holder():
+    name = input('What is the name of daredevil? ')
+    country = input('What country does this person come from? ')
+    record = input('What is their record for catches? ')
+    print()
+    if len(name)<2 or len(country)<4 or record.isnumeric==False or len(record)==0:
+        print('Please enter full name, country name and a number to input a record.\n')
+        return
+    juggler = Records(name=name, country=country, record=record)
+    juggler.save()
+    print(f"{juggler.name}s' has been saved")
 
-def search_for_RH():
+# def search_for_RH():
 
-def update_record():
 
-def delete_record():
+# def update_record():
+
+# def delete_record():
 
 if __name__ == '__main__':
     main()
